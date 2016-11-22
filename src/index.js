e72a1aff1ed218809f9b91af9de233e7d29ceae4
Denis <skunshd@gmail.com>
2016-11-22 10:34:58 +0100

    Entrance point

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-
 import { Provider } from 'react-redux'
-// Итак, первый компонент из мира Redux - <Provider />
-// Благодаря этому компоненту, мы сможем получать необходимые данные из store нашего приложения, 
-// если воспользуемся вспомогательной функцией connect
 
 import App from './containers/App'
 import { AppContainer } from 'react-hot-loader'
 
 import './styles/app.css'
 import configureStore from './store/configureStore'
-
-// Пока мы писали код для асинхронного запроса, мы НЕ нарушили главные принципы redux-приложения:
-// 1) Мы всегда возвращали новое состояние (новый объект, смотрите src/reducers/page.js)
-// 2) Мы строго следовали однонаправленному потоку данных в приложении: юзер кликнул - возникло действие - редьюсер изменил - компонент отобразил.
 
 const store = configureStore()
 
@@ -31,7 +23,6 @@ render(
   document.getElementById('root')
 )
 
-// Hot Module Replacement API
 if (module.hot) {
   module.hot.accept('./containers/App', () => {
     const NextApp = require('./containers/App').default;
